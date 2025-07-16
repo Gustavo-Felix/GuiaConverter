e62e141aaaac529c96573fb9716832e49e2edfab
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-07-16 06:39:30 -0300

    Escrita de arquivos

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const fs = require('fs');
 
-var conteudo;
-
-fs.readFile('./gustavo.felix', {encoding: 'utf-8'}, (erro, dados) => {
-    if (erro) {
-        console.log('Ocorreu uma falha ao ler o arquivo')
-    }
-    else {
-        console.log(dados)
-        conteudo = dados
-        console.log(conteudo)
+fs.writeFile('./gustavo.felix', "Novo conteúdo do arquivo!", (err) => {
+    if (err) {
+        console.log('Ocorreu uma falha durante o salvamento')
     }
 });
