2ccba82cb32384ceca84404bc0cf8f01fb6a48a6
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-07-16 07:32:58 -0300

    Table

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var Reader = require('./Reader')
 var Processor = require('./Processor')
+var Table = require('./Table')
 
 var leitor = new Reader
 
@@ -7,6 +8,11 @@ var leitor = new Reader
 async function main() {
     var data = await leitor.Read('./users.csv')
     var dadosProcessados = Processor.Process(data)
+
+    var users = new Table(dadosProcessados)
+
+    console.log(users.header)
+    console.log(users.rows)
     
 }
 
