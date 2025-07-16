f80576a66b83ea9672fd298ea8adb7d177a0c0ea
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-07-16 06:48:56 -0300

    Leitor/Escritor de JSON

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const fs = require('fs');
 
-fs.writeFile('./gustavo.felix', "Novo conteúdo do arquivo!", (err) => {
-    if (err) {
-        console.log('Ocorreu uma falha durante o salvamento')
-    }
-});
+function modificarArquivo(nome, curso, categoria){
+    fs.readFile('./usuario.json', {encoding: 'utf-8'}, (err, dados) => {
+        if(err){
+            console.log("Um erro aconteceu!")
+        } else {
+            var conteudo = JSON.parse(dados) // Texto para objeto Javascript(JSON)
+            console.log(conteudo)
+            conteudo.nome = nome
+            conteudo.curso = curso
+            conteudo.categoria = categoria
+            console.log(conteudo)
+
+            fs.writeFile('./usuario.json', JSON.stringify(conteudo), (err) => { // Objeto Javascript(JSON) para texto
+                if (err) {
+                    console.log("Ocorreu um erro")
+                }
+                
+            }) 
+        }
+    });
+}
+
+modificarArquivo("José", "Formação Node", "Node.js");
